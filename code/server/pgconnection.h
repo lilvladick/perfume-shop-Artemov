7b9d8d79377f7835c1d6d0183d9c754e603c87b9
Vladick <vlad2004ki@gmail.com>
2024-03-14 20:17:07 +0300

    upd pgconnection.h file

--- code/server/pgconnection.h
+++ code/server/pgconnection.h
@@ -1,28 +1,22 @@
 #ifndef PGCONNECTION_H
 #define PGCONNECTION_H
 
-#include <memory>
-#include <mutex>
+#include <memory.h>
+#include <mutex.h>
 #include <libpq-fe.h>
 
-class PGConnection
+typedef struct PGConnection
 {
-public:
-    PGConnection();
-    std::shared_ptr<PGconn> connection() const;
+    char* dbhost;
+    int dbport;
+    char* dbname;
+    char* dbuser;
+    char* dbpass;
+    PGconn* connection;
+} PGConnection;
 
-private:
-    void establish_connection();
-
-    std::string m_dbhost = "82.179.140.18";
-    int         m_dbport = 5432;
-    std::string m_dbname = "p2206_perfume_shop";
-    std::string m_dbuser = "mpi";
-    std::string m_dbpass = "135a1";
-
-    std::shared_ptr<PGconn>  m_connection;
-
-};
-
+PGConnection* PGConnection_create();
+PGconn* PGConnection_connection(const PGConnection* pgConn);
+void PGConnection_destroy(PGConnection* pgConn);
 
 #endif //PGCONNECTION_H
