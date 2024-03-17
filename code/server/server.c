766841bd0ac22d56bcd394b894a3508e66a0eaec
Vladick <vlad2004ki@gmail.com>
2024-03-17 22:50:49 +0300

    nerd server closed txt & server.c update

--- code/server/server.c
+++ code/server/server.c
@@ -10,38 +10,98 @@
 #include "httpd.h"
 #include "pg.h"
 
-int main() {
+static int _stopFlag = 0;
+const static int _listenCount = 5;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stdout, "Please, write correct port argument.\n");
+        return 1;
+    }
+    char buf[2048];
+
     int server = Socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in adr = {0};
     adr.sin_family = AF_INET;
-    adr.sin_port = htons(25522);
+    adr.sin_port = htons(atoi(argv[1]));
     Bind(server, (struct sockaddr *) &adr, sizeof adr);
+    socklen_t addrLen = sizeof adr;
 
     Listen(server, 5);
+    fprintf(stdout, "Listening on port: %s \n", argv[1]);
 
-    socklen_t adrlen = sizeof adr;
-    int fd = Accept(server, (struct sockaddr *) &adr, &adrlen);
+    while (!_stopFlag) {
+        pid_t pid;
+        int fd;
+        ssize_t nRead;
+        
+        if ((fd = Accept(server, (struct sockaddr *) &adr, &addrLen)) < 0 && errno != EINTR) {
+            fprintf(stderr, "Unable to accept client: %s\n", strerror(errno));
+        } else if (!_stopFlag) {
+            fprintf(stdout, "Accepted client from %s\n", inet_ntoa(adr.sin_addr));
+            
+            nRead = read(fd, buf, sizeof(buf));
+            if (nRead == -1) {
+                perror("Read failed");
+                exit(EXIT_FAILURE);
+            }
+            if (nRead == 0) {
+                printf("End of file occurred\n");
+            }
+            buf[nRead] = '\0';
 
-    ssize_t nread;
-    char buf[256];
-    nread = read(fd, buf, 256);
-    if (nread == -1) {
-        perror("Read failed");
-        exit(EXIT_FAILURE);
+            // Парсим HTTP-запрос, извлекаем метод и URL
+            char method[5];
+            char url[128];
+            sscanf(buf, "%4s %127s", method, url);
+            char* body = strstr(buf, "\r\n\r\n");
+
+            // Простой роутер для метода GET
+            if (strncmp(method, "POST", 4) == 0 && body != NULL) {
+                body += 4;
+                if (strncmp(url, "/get_data", 9) == 0) {
+                    handle_post(fd, body);
+                } else {
+                    handle_not_found(fd);
+                }
+            } else {
+                // Добавьте обработку других методов или роутов по мере необходимости
+                write(fd, "HTTP/1.1 405 Method Not Allowed\r\n\r\n", 35);
+            }
+            close(fd);
+        }
+
     }
-    if (nread == 0) {
-        printf("End of file occured\n");
-    }
-
-    write(STDOUT_FILENO, buf, nread);
-    write(fd, buf, nread);
 
     sleep(1);
 
-    close(fd);
     close(server);
 
-    printf("BYE BYE");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⣀⣠⣀⣀⣄⣀⣤⣀⣄⣠⣀⣄⣤⣿⣿⣿⣿⣿⣿⣿⣿⣟⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣘⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⣄⣀⣄⣀⣠⣀⣀⣄⣠⣠⣄⣄⡀
+        ⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
+        ⠀⠀⠀⢹⣿⣿⡟⠋⠙⠉⠋⠙⠉⠋⠙⠉⣋⡙⠉⠋⠙⠉⠋⠙⠉⠋⠹⣿⣿⣿⣿⡟⠛⠙⠋⠛⠙⠋⠛⠉⠋⣙⡉⠋⠙⠉⠋⠙⠉⠋⠙⠙⣿⣿⣿⠁⠀⠀
+        ⠀⠀⠀⠀⣿⣿⣿⠀⠀⢀⠂⠈⢠⠐⣥⢋⡴⣻⣿⡗⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣷⠀⠀⠀⠠⠀⠈⢰⣟⣧⠀⠀⠀⠁⠀⠀⠀⡀⠀⠀⢠⣿⣿⡏⠀⠀⠀
+        ⠀⠀⠀⠀⢿⣿⣿⡀⠀⠆⠀⠠⠸⠀⢼⣮⣿⢿⡽⣧⠀⢡⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⡆⠀⠀⡁⠐⠐⢸⡎⣽⣧⢀⠀⠀⠀⠁⠀⠀⠀⠀⣸⣿⣿⠃⠀⠀⠀
+        ⠀⠀⠀⠀⠸⣿⣿⣇⠀⠀⠀⢰⣁⣬⡟⣿⢞⣯⣿⡷⣐⢦⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣷⠀⠀⠈⢦⣕⡷⣭⣿⣾⡔⡌⠠⣄⠘⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⣿⣿⣿⡀⠀⠀⠈⢛⡶⣽⢯⣿⣯⣿⣿⣽⠞⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠘⢿⣿⣿⣿⣿⣿⣜⠷⢡⠊⠀⠀⠀⣰⣿⣿⡏⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠘⣿⣿⣷⣄⠀⠀⠀⠙⠫⢿⡿⣿⠿⠟⠁⠀⠀⣠⣾⣿⣿⡟⢣⠚⡽⣿⣿⣿⣆⡀⠀⠈⠹⢿⣿⣿⣿⠯⠛⠁⠀⠀⢀⣴⣿⣿⡿⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣷⣶⣤⣀⣀⣀⣀⣀⣀⣀⣠⣴⣾⣿⣿⣿⠟⠈⠀⠐⠈⠹⢿⣿⣿⣷⣦⣄⣀⣀⣠⣀⣀⣀⣀⣠⣤⣶⣿⣿⡿⠏⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠁⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⡤⠉⠈⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⢉⡉⠁⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⠈⠁⠀⠀⠀⠐⣝⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢢⠀⠀⠸⠜⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⢂⠀⠀⠀⠤⡀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣦⠖⠰⠀⠆⠰⠀⠆⠄⠲⠴⣶⣤⡀⠀⠀⠈⠃⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠑⠁⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⣿⣿⠇⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢆⠀⠀⠀⠀⠀⠀⠀⠀⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠈⠒⠐⠂⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢡⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⡀⠄⢀⠠⠀⡀⠀⠄⡀⢀⠀⣀⠔⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠠⢅⢂⠰⣀⠂⢄⠡⠈⠄⡐⠠⠐⣀⢂⡐⢠⢁⣒⡰⠜⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⣈⠗⣦⢯⣴⣌⣱⣌⣰⣡⣣⣔⣦⡼⣖⠫⠔⡠⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠐⠠⠉⠄⠓⠌⠫⢙⠋⠟⠹⢩⠟⠌⠱⠈⠁⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n
+        server closed\n");
 
     return 0;
 }
