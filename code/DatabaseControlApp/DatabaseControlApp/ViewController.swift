4b9a9e663547fe7a14ee0ff60dfe2900f2db8780
Владислав <vlad2004ki@gmail.com>
2024-04-02 20:52:56 +0300

    modal window and pop up button
    не работает кнопка закрытия в модалке =(

--- code/DatabaseControlApp/DatabaseControlApp/ViewController.swift
+++ code/DatabaseControlApp/DatabaseControlApp/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var selectTableButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setSelectTableButton()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: actions
+    @IBAction func GoExit(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
-
+    //MARK: functions
+    func setSelectTableButton() {
+        let optionClose = {(action: UIAction) in
+            print(action.title)
+        }
+        
+        if let selectTableButton = selectTableButton {
+            selectTableButton.menu = UIMenu(children:[
+                UIAction(title: "tab1", state: .on, handler: optionClose),
+                UIAction(title: "tab2", handler: optionClose),
+                UIAction(title: "tab3", handler: optionClose)
+            ])
+            selectTableButton.showsMenuAsPrimaryAction = true
+            selectTableButton.changesSelectionAsPrimaryAction = true
+        } else {
+            print("selectTableButton is nil")
+        }
+    }
+    
+    
 }
+
 
